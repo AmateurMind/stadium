1b2cd8d3bb2088710aea3e95b460d83d6a9ef5f3
AmateurMind <suhail17mohammad@gmail.com>
2026-07-11 21:49:57 +0530

    SEC CHANGES

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -37,7 +37,7 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers["Content-Security-Policy"] = (
             "default-src 'self'; "
             "script-src 'self'; "
-            "style-src 'self' 'unsafe-inline'; "
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "img-src 'self' data:; "
             "connect-src 'self'; "
             "font-src 'self' https://fonts.gstatic.com"
